e365002be4b0cb4fbf3563ba09c784f223497e99
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-19 10:22:21 -0300

    Adicionando Atributos Conta

---
+++ Classes/Conta.cs
@@ -0,0 +1,13 @@
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        // Atributos
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+    }
+
+}
