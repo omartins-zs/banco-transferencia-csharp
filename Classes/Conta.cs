6e991620c8e3f19b553f3248cce159c2c70dadd8
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-20 08:45:25 -0300

    Adicionando metodo Transferir

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -41,5 +41,11 @@ namespace DIO.Bank
 
             Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
 		}
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+		{
+			if (this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+		}
     }
 }
