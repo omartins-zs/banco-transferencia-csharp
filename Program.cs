1b09b0b9ad1e52dcf1e32ab5a7dada8feabc2d8a
omartins-zs <gabrielmatheus209@gmail.com>
2021-10-20 10:21:57 -0300

    Adicionando Inserir Conta

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
@@ -41,6 +42,30 @@ namespace DIO.Bank
 			
 			Console.WriteLine("Obrigado por utilizar nossos serviços.");
 			Console.ReadLine();
+		}
+
+        private static void InserirConta()
+		{
+			Console.WriteLine("Inserir nova conta");
+
+			Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+			int entradaTipoConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Nome do Cliente: ");
+			string entradaNome = Console.ReadLine();
+
+			Console.Write("Digite o saldo inicial: ");
+			double entradaSaldo = double.Parse(Console.ReadLine());
+
+			Console.Write("Digite o crédito: ");
+			double entradaCredito = double.Parse(Console.ReadLine());
+
+			Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+										saldo: entradaSaldo,
+										credito: entradaCredito,
+										nome: entradaNome);
+
+			listContas.Add(novaConta);
 		}
 
         private static string ObterOpcaoUsuario()
